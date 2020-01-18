d7907cbc01d09affb8220028a44cb9a51c53f598
Robert Notwicz <rob.notwicz@gmail.com>
2020-01-18 02:51:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -154,6 +154,7 @@ def get_player_by_highest_stat(game)
   pp all_players
   base = all_players[0]
   all_players.reduce(base) do |player|
+    puts "#{base} ;;;; #{player}"
     base = yield(base, player)
   end
 end
