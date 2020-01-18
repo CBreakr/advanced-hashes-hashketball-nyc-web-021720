99b1f0f91a62dcc390e498ab95ab3e59bc33970c
Robert Notwicz <rob.notwicz@gmail.com>
2020-01-18 02:50:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -162,6 +162,8 @@ end
 def big_shoe_rebounds
   game = game_hash
   player = get_player_by_highest_stat(game) do |current_player, next_player|
+    puts "current: #{current_player}"
+    puts "next: #{next_player}"
     next_player[:shoe] > current_player[:shoe] ? next_player : current_player
   end
   player ? player[:rebounds] : nil
